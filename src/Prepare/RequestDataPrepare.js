af15d7f8f933832dd50c3dba356fb2e3d0deaccc
Andrey Tyurin <ectb08@mail.ru>
2021-07-04 16:43:46 +0300

    deleting user settings from a folder src

--- src/Prepare/RequestDataPrepare.js
+++
@@ -1,7 +0,0 @@
-
-// eslint-disable-next-line no-unused-vars
-const RequestDataPrepare = (requestType, requestUrl, requestData) => {
-  return requestData;
-};
-
-export default RequestDataPrepare;
--- src/User/ShowErrorMessage.js
+++
@@ -1,6 +0,0 @@
-
-const ShowErrorMessage = (message) => {
-  global.VueApp && global.VueApp.$dialogs && global.VueApp.$dialogs.alert(message, {title: 'Ошибка'});
-};
-
-export default ShowErrorMessage;
