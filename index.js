4b1206719a21f49660a62117650f74b01226326a
Andrey Tyurin <ectb08@mail.ru>
2022-04-15 04:22:42 +0300

    create alpha version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-import RequestManagerCreate from './src/Core/RequestManagerCreate';
-import _MethodInfo          from './src/Class/MethodInfo';
+import RequestManagerCreate     from './src/Core/RequestManagerCreate';
+import _MethodInfo              from './src/Class/MethodInfo';
+import _RequestManagerException from './src/Exception/RequestManagerException';
 
-export const RequestManager = RequestManagerCreate;
-export const MethodInfo     = _MethodInfo;
+export const RequestManager           = RequestManagerCreate;
+export const MethodInfo               = _MethodInfo;
+export const RequestManagerException  = _RequestManagerException;
